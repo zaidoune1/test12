8d78ba381036029ca98aa9bd6a7f8d0c48110ce2
zaidoune1 <81720068+zaidoune1@users.noreply.github.com>
2024-11-29 18:31:44 +0100

    dqqq

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h3>dqdqqsd</h3>
-      <h3>dqdqqsd</h3>
-      <h3>dqdqqsd</h3>
-      <h3>dqdqqsd</h3>
-      <h3>dqdqqsd</h3>
-      <h3>dqdqqsd</h3>
-
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h1> moezmoezmoezmoez </h1>
-      <h2>dhqsjdqndq</h2>
-      <h2>dhqsjdqndq</h2>
-      <h2>dhqsjdqndq</h2>
-      <h2>dhqsjdqndq</h2>
-      <h2>dhqsjdqndq</h2>
+      <h1> lolo </h1>
     </>
   );
 }
