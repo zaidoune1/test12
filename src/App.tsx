c4bb311575ffa84a3fdac7b2745ec545a5c60c4d
zaidoune1 <81720068+zaidoune1@users.noreply.github.com>
2024-12-01 17:05:03 +0100

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,195 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>c<h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-      <h1> devolop </h1>
-    </>
+    <div>
+      <div className="scrollign_containt">
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt
+          </p>
+        </div>
+        <div className="card">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt
+          </p>
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </div>{" "}
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </div>{" "}
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </div>
+      </div>
+      <div
+        style={{
+          margin: "20px auto",
+          width: "200px",
+          height: "200px",
+          backgroundColor: "royalblue",
+          borderRadius: "10px",
+          padding: "5px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          resize: "both",
+          overflow: "auto",
+        }}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        </p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div className="dropdown_wrapper">
+          <button className="block_drop">drop</button>
+          <div className="drop_down">
+            <button>moez</button>
+            <button>ok fine</button>
+            <button>ok ok</button>
+          </div>
+        </div>
+        <h1> moez </h1>
+        <button className="btn"> Test </button>
+        <button className="btn-shadow"> shadow hover effect </button>
+        <label htmlFor="myCheckbox">my label</label>
+        <input type="checkbox" id="myCheckbox" />
+
+        <div
+          className="container"
+          style={{
+            border: "1px solid black",
+            padding: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <p>moez zaidoune from css develepment</p>
+          <p>moez zaidoune from css develepment</p>
+          <p>moez zaidoune from css develepment</p>
+        </div>
+
+        <div
+          style={{
+            backgroundColor: "rosybrown",
+            color: "white",
+            padding: "10px",
+            borderRadius: "10px",
+            marginBottom: "20px",
+          }}
+          className="container_texte"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+
+        <div
+          style={{
+            backgroundColor: "rosybrown",
+            color: "white",
+            padding: "10px",
+            borderRadius: "10px",
+            marginBottom: "20px",
+          }}
+          className="container_clamp"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+
+        <div
+          style={{
+            backgroundColor: "rosybrown",
+            color: "white",
+            borderRadius: "10px",
+            marginBottom: "20px",
+          }}
+          className="container_ombre"
+        >
+          <div className="transparent">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+
+        <div
+          style={{
+            backgroundColor: "rosybrown",
+            color: "white",
+            borderRadius: "10px",
+            marginBottom: "20px",
+          }}
+          className="container_select_box"
+        >
+          <select>
+            <option value="white">light mode</option>
+            <option value="black">dark_mode</option>
+          </select>
+        </div>
+
+        <h1 className="grendient_text"> js full stack devlopper </h1>
+      </div>
+    </div>
   );
 }
 
